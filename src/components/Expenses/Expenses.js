59409e29d50826ab9dea27bd6917d983cf57a0e5
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2022-11-10 21:46:31 +0530

    Render list dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,26 +16,13 @@ const Expenses = props => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          date={props.items[0].date}
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          date={props.items[1].date}
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-        />
-        <ExpenseItem
-          date={props.items[2].date}
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-        />
-        <ExpenseItem
-          date={props.items[3].date}
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-        />
+        {props.items.map(expense => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
